ec7e6d7fbd1681602b96ddca6c0d4fafd04e6aaa
lpeterduke <lpeterduke@gmail.com>
2016-10-08 16:13:57 +0800

    1613

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/MainActivity.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
@@ -9,18 +9,14 @@ import android.widget.Toolbar;
  * Created by Peterli on 10/7/16.
  */
 
-public class homeActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
-
+public class homeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-        
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
     }
 
 
