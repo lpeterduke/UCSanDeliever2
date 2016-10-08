0ec3f2167dc5b326c64886c24f839f36f7279676
lpeterduke <lpeterduke@gmail.com>
2016-10-08 14:25:04 +0800

    14;24 xixi

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
@@ -2,17 +2,25 @@ package edu.ucsd.cse110.ucsandeliever;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
 
 /**
  * Created by Peterli on 10/7/16.
  */
 
-public class homeActivity extends Activity {
+public class homeActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
+        
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
