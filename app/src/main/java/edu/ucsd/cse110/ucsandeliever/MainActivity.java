6d84b202bd7ec114c951129b9214a251f5b02b37
lpeterduke <lpeterduke@gmail.com>
2016-10-08 16:46:14 +0800

    1613

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
 
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 
--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/homeActivity.java
@@ -3,21 +3,33 @@ package edu.ucsd.cse110.ucsandeliever;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 
 /**
  * Created by Peterli on 10/7/16.
  */
 
-public class homeActivity extends Activity {
+public class homeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
 
-
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setTitle(R.string.my_tb_title);
+        getSupportActionBar().setIcon(R.drawable.toolbar_add);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
