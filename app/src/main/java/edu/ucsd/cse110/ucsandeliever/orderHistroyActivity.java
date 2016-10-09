ce21d6b6d36ff24abfc9592439c813c3288ad46d
lpeterduke <lpeterduke@gmail.com>
2016-10-09 13:05:58 +0800

    1128

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/orderHistroyActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/orderHistroyActivity.java
@@ -1,61 +1,56 @@
 package edu.ucsd.cse110.ucsandeliever;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.Toast;
 
 /**
- * Created by Peterli on 10/7/16.
+ * Created by Administrator on 2016/10/9.
  */
 
-public class homeActivity extends AppCompatActivity {
+public class orderHistroyActivity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.orderhistroy_layout);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_layout);
 
         Toolbar my_toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(my_toolbar);
         getSupportActionBar().setIcon(R.drawable.acct_info);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()){
             case R.id.menu_1:
+                Toast.makeText(orderHistroyActivity.this, "Request Order",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(this,requestActivity.class);
                 startActivity(intent);
                 break;
 
             case R.id.menu_2:
+                Toast.makeText(orderHistroyActivity.this, "My Order Status",Toast.LENGTH_SHORT).show();
                 Intent intent2 = new Intent(this,orderStatus.class);
                 startActivity(intent2);
                 break;
-
-            case R.id.menu_3:
-                Intent intent3 = new Intent(this,orderHistroyActivity.class);
-                startActivity(intent3);
-                break;
         }
-
-
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/orderStatus.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/orderStatus.java
@@ -1,18 +1,55 @@
 package edu.ucsd.cse110.ucsandeliever;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016/10/8.
  */
 
-public class orderStatus extends Activity{
+public class orderStatus extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         setContentView(R.layout.orderstatus_layout);
         super.onCreate(savedInstanceState);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setIcon(R.drawable.acct_info);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_1:
+                Toast.makeText(orderStatus.this, "Request Order",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this,requestActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.menu_3:
+                Toast.makeText(orderStatus.this, "Order history",Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this,orderHistroyActivity.class);
+                startActivity(intent3);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
