4b86d4fc8f995b9aee588e92a40d888c272848d6
lpeterduke <lpeterduke@gmail.com>
2016-10-31 07:41:59 +0800

    1641 xixi

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/requestActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/requestActivity.java
@@ -1,14 +1,18 @@
 package edu.ucsd.cse110.ucsandeliever;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,8 +20,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.Button;
 
@@ -35,7 +42,10 @@ import com.google.firebase.auth.FirebaseUser;
  * Created by Administrator on 2016/10/8.
  */
 
-public class requestActivity extends Fragment{
+public class requestActivity extends Fragment implements TimePickerDialog.OnTimeSetListener{
+
+    Button b_pick;
+
 
     Spinner spinner;
     ArrayAdapter<CharSequence> adapter;
@@ -46,17 +56,38 @@ public class requestActivity extends Fragment{
     private EditText etDestination;
     private Button makeOrder;
     private   EditText etPid;
+    private EditText timeFromClock;
 
     final Firebase myFirebaseRef = new Firebase("https://uc-student-deliver.firebaseio.com/");
 
+    TimePickerDialog.OnTimeSetListener onTimeSetListener;
 
-
+int hour,minute;
 
     View myView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.request_layout, container, false);
+        timeFromClock = (EditText)myView.findViewById(R.id.editText3);
+        b_pick = (Button) myView.findViewById(R.id.timePicker);
+        b_pick.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                java.util.Calendar calendar = java.util.Calendar.getInstance();
+                hour = calendar.get(java.util.Calendar.HOUR_OF_DAY);
+                minute = calendar.get(java.util.Calendar.MINUTE);
+
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),requestActivity.this,hour,minute,true);
+                timePickerDialog.show();
+
+
+
+
+            }
+        });
 
         spinner = (Spinner) myView.findViewById(R.id.spinner_restaurant);
         adapter = ArrayAdapter.createFromResource(getActivity(),R.array.restaurant_names,android.R.layout.simple_spinner_item);
@@ -98,7 +129,7 @@ public class requestActivity extends Fragment{
 
         //get Input
         etItemName = (EditText) myView.findViewById(R.id.editText8); // get user Email
-        etTime  = (EditText) myView.findViewById(R.id.editText9); // get user password
+       etTime  =  timeFromClock; // get user password
         etDestination  = (EditText) myView.findViewById(R.id.editText10); // get user password
 
        // etPid = (EditText) myView.findViewById(R.id.editText6);
@@ -137,7 +168,16 @@ public class requestActivity extends Fragment{
     }
 
 
+    @Override
+    public void onTimeSet(TimePicker timePickerDialog, int hourOfDay, int minute) {
+
+        System.out.println("----------------------------------"+hourOfDay+minute+"---------");
 
 
+        Toast.makeText(getActivity(),"Hour: "+hourOfDay + " Minute: " + minute,Toast.LENGTH_SHORT);
+
+        timeFromClock.setText(hourOfDay+" : "+ minute);
+
+    }
 
 }
