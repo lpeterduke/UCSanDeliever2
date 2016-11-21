9ac4293b045a0fbcbcd844af6bb04aa95f4a02ee
lpeterduke <lpeterduke@gmail.com>
2016-11-20 23:12:49 -0800

    xixi debug for he

--- app/src/main/java/edu/ucsd/cse110/ucsandeliever/requestor_contactActivity.java
+++ app/src/main/java/edu/ucsd/cse110/ucsandeliever/requestor_contactActivity.java
@@ -20,110 +20,122 @@ public class requestor_contactActivity extends Activity {
 
     private String balance1 = "";
     private String balance2 = "";
+
+    private Button reveived;
+    private Button contRunner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_requestor_contact);
 
+
+        reveived = (Button) findViewById(R.id.buttonReceived);
+        reveived.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // finishing page
+                DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference ordersRef = mRootRef.child("orders");
+
+                System.out.println("is about to end");
+
+
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+                final String requester = currentUser.getUid();
+
+                Intent i = getIntent();
+                Bundle data = i.getExtras();
+                final String runner = data.getString("runnerGet");;
+                int changedValue = 0;
+                DatabaseReference ref1 =FirebaseDatabase.getInstance().getReference().child("users").child(runner).child("balance");
+                FirebaseDatabase.getInstance().getReference().child("users").child(runner).child("runnerStatusIndicator").setValue(false);
+
+                DatabaseReference ref2 =FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("balance");
+                FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("alreadyPick").setValue(false);
+                FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("requesting").setValue(false);
+
+                ref1.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+                    @Override
+                    public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+
+                        System.out.println("余额变更");
+                        String msg = dataSnapshot.getValue(String.class);
+                        System.out.println("msg: " + msg);
+
+                        balance1 = msg;
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+                ref2.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+                    @Override
+                    public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+
+                        System.out.println("余额变更");
+                        String msg = dataSnapshot.getValue(String.class);
+                        System.out.println("msg: " + msg);
+                        balance2 = msg;
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                //这段代码有问题 comment掉就可以 进到下一页
+
+               // int newBalanceForUser1 = Integer.parseInt(balance1) + changedValue;
+            //    int newBalanceForUser2 = Integer.parseInt(balance2) - changedValue;
+               // ref1.setValue(Integer.toString(newBalanceForUser1));
+              //  ref2.setValue(Integer.toString(newBalanceForUser2));
+
+
+
+                Intent intent = new Intent(requestor_contactActivity.this,requestor_finishActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        contRunner = (Button) findViewById(R.id.buttonContRunner);
+        contRunner.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+
+                System.out.println("is about to chat");
+                // chatting page for Pan
+                Intent i = getIntent();
+                Bundle data = i.getExtras();
+                String Runneuid = data.getString("runnerGet");
+
+                Intent k = new Intent(requestor_contactActivity.this,UserList.class);
+                Bundle b = new Bundle();
+                b.putString("runnerGet", Runneuid);
+                k.putExtras(b);
+                startActivity(k);
+            }
+        });
+
+
     }
 
-
-    public void welcomeSystem(View view){
-
-        String button_text;
-        button_text = ((Button) view).getText().toString();
-
-        if(button_text.equals("Contact the Runner")){
-
-            System.out.println("is about to chat");
-            // chatting page for Pan
-            Intent i = getIntent();
-            Bundle data = i.getExtras();
-            String Runneuid = data.getString("runnerGet");
-
-            Intent k = new Intent(requestor_contactActivity.this,UserList.class);
-            Bundle b = new Bundle();
-            b.putString("runnerGet", Runneuid);
-            k.putExtras(b);
-            startActivity(k);
-
-        }else if(button_text.equals("I have received the order")){
-
-            // finishing page
-            DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-            DatabaseReference ordersRef = mRootRef.child("orders");
-
-            System.out.println("is about to end");
-
-
-            FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-            FirebaseUser currentUser = mAuth.getCurrentUser();
-
-
-            final String requester = currentUser.getUid();
-
-            Intent i = getIntent();
-            Bundle data = i.getExtras();
-            final String runner = data.getString("runnerGet");;
-            int changedValue = 0;
-            DatabaseReference ref1 =FirebaseDatabase.getInstance().getReference().child("users").child(runner).child("balance");
-            FirebaseDatabase.getInstance().getReference().child("users").child(runner).child("runnerStatusIndicator").setValue(false);
-
-            DatabaseReference ref2 =FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("balance");
-            FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("alreadyPick").setValue(false);
-            FirebaseDatabase.getInstance().getReference().child("users").child(requester).child("requesting").setValue(false);
-
-            ref1.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
-                @Override
-                public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
-
-                    System.out.println("余额变更");
-                    String msg = dataSnapshot.getValue(String.class);
-                    System.out.println("msg: " + msg);
-
-                    balance1 = msg;
-
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-
-            ref2.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
-                @Override
-                public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
-
-                    System.out.println("余额变更");
-                    String msg = dataSnapshot.getValue(String.class);
-                    System.out.println("msg: " + msg);
-                    balance2 = msg;
-
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-            int newBalanceForUser1 = Integer.parseInt(balance1) + changedValue;
-            int newBalanceForUser2 = Integer.parseInt(balance2) - changedValue;
-
-            ref1.setValue(Integer.toString(newBalanceForUser1));
-            ref2.setValue(Integer.toString(newBalanceForUser2));
-
-
-
-            Intent intent = new Intent(this,requestor_finishActivity.class);
-            startActivity(intent);
-        }
-
-    }
 
     @Override
     public void onBackPressed() {
